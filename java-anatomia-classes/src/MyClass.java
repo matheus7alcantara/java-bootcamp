9642da65250f586ed3d3caed99b2fd3f49b02738
matheus7alcantara <f.matheus10@gmail.com>
2023-08-18 17:52:27 -0300

    First Commit

---
+++ java-anatomia-classes/src/MyClass.java
@@ -0,0 +1,12 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.SystemMenuBar;
+
+public class MyClass { //Use PascalCase for file names
+    
+public static void main (String [] args) {
+
+    
+
+
+}
+
+}
