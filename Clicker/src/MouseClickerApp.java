84a7bbd6cc1b119d48bc9f881df9d05fea4d4565
matheus7alcantara <f.matheus10@gmail.com>
2023-08-18 13:10:42 -0300

    feature: input number of clicks

--- Clicker/src/MouseClickerApp.java
+++ Clicker/src/MouseClickerApp.java
@@ -2,12 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.InputEvent; // Add this import for InputEvent
+import java.awt.event.InputEvent;
 
 public class MouseClickerApp {
     private static boolean isRunning = false;
+    private static int clickInterval = 1; // Default click interval in seconds
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> createAndShowGUI());
@@ -18,13 +17,16 @@ public class MouseClickerApp {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(4, 2, 10, 10));
+        panel.setLayout(new GridLayout(5, 2, 10, 10));
 
         JLabel xLabel = new JLabel("X Coordinate:");
         JTextField xTextField = new JTextField();
 
         JLabel yLabel = new JLabel("Y Coordinate:");
         JTextField yTextField = new JTextField();
+
+        JLabel intervalLabel = new JLabel("Click Interval (seconds):");
+        JTextField intervalTextField = new JTextField(String.valueOf(clickInterval));
 
         JButton startButton = new JButton("Start Clicking");
         JButton stopButton = new JButton("Stop Clicking");
@@ -34,6 +36,8 @@ public class MouseClickerApp {
         panel.add(xTextField);
         panel.add(yLabel);
         panel.add(yTextField);
+        panel.add(intervalLabel);
+        panel.add(intervalTextField);
         panel.add(startButton);
         panel.add(stopButton);
 
@@ -51,9 +55,11 @@ public class MouseClickerApp {
                     stopButton.setEnabled(true);
                     String xText = xTextField.getText();
                     String yText = yTextField.getText();
+                    String intervalText = intervalTextField.getText();
 
                     int xCoord = Integer.parseInt(xText);
                     int yCoord = Integer.parseInt(yText);
+                    clickInterval = Integer.parseInt(intervalText);
 
                     RobotClicker robotClicker = new RobotClicker(xCoord, yCoord);
                     Thread clickerThread = new Thread(robotClicker);
@@ -98,7 +104,7 @@ public class MouseClickerApp {
                     robot.mouseMove(x, y);
                     robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
                     robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-                    Thread.sleep(100); // Click every 100 milliseconds
+                    Thread.sleep(clickInterval * 1000); // Click with specified interval
                 }
             } catch (AWTException | InterruptedException ex) {
                 // Do nothing, just stop the clicking loop
